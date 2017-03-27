a5468f89ad5a236c183c79da58c1c66189296699
GomuGomuMan <lucky7duy211@gmail.com>
2017-03-26 19:41:37 -0700

    Extracted school to json

--- schoolList.js
+++ schoolList.js
@@ -1,3 +1,41 @@
 /**
  * Created by LUCKY on 3/26/2017.
  */
+
+var csv = require('csv');
+var fs = require('graceful-fs');
+
+const CSV_PATH = 'resources/pubschls.csv';
+
+
+
+function extractSchool(err, data) {
+    if (err)
+      console.log("Error: " + err);
+    else {
+      var jsonSchool = [];
+
+      data.forEach(function (row) {
+          //console.log("School: " + row.School);
+          if (row.School)
+            jsonSchool.push(row.School);
+      });
+      console.log(jsonSchool);
+
+      //TODO: Print to json file
+    }
+}
+
+
+
+function readCSV() {
+    var readStream = fs.createReadStream(CSV_PATH);
+    parser = csv.parse({columns: true}, extractSchool);
+    readStream.pipe(parser);
+}
+
+
+
+if (require.main === module) {
+    readCSV();
+}
