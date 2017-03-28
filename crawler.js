6c3f9957a2d38955188e42b625526923ab6efc8b
GomuGomuMan <lucky7duy211@gmail.com>
2017-03-27 18:40:07 -0700

    Ignore .png

---
+++ crawler.js
@@ -0,0 +1,106 @@
+/**
+ * Created by LUCKY on 3/26/2017.
+ */
+//TODO: Finish crawler's skeleton
+var Horseman = require('node-horseman');
+var fs = require('graceful-fs');
+
+const USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0';
+const URL = 'http://data1.cde.ca.gov/dataquest/page2.asp?level=School&subject=LC&submit1=Submit';
+const SELECTOR_SCHOOL = 'input[name="cName"]';
+const SELECTOR_FEP = 'input[value="Redesig4"]';
+const SELECTOR_SUBMIT = '#submit1';
+const HTML_PATH = 'output/doc.html';
+
+function configHorseman(Horseman) {
+    var horseman = new Horseman({
+        loadImages: false,
+        timeout: 10000
+    });
+
+    return horseman;
+    // fsmHorseman(horseman);
+}
+
+function fsmHorseman(horseman, callback) { // Finite State Machine Horseman
+    horseman
+        .userAgent(USER_AGENT)
+        .open(URL)
+        .status()
+        .then(checkStatus)
+        //1st page
+        .exists(SELECTOR_SCHOOL)
+        .then(checkSelectorExist)
+        .type(SELECTOR_SCHOOL, 'Franklin Elementary') // input school //TODO: limit to 25 char and text from json
+        .keyboardEvent('keypress', 16777221) // type enter
+        .waitForNextPage() // wait for loading
+        .status()
+        .then(checkStatus)
+        //2nd page
+        // Check FEP
+        .exists(SELECTOR_FEP)
+        .then(checkSelectorExist)
+        .click(SELECTOR_FEP)
+        // Click submit btn
+        .exists(SELECTOR_SUBMIT)
+        .then(checkSelectorExist)
+        .click(SELECTOR_SUBMIT)
+        .waitForNextPage()
+        .status()
+        .then(checkStatus)
+        //result page
+        .screenshot('test.png') // Success!!
+        .url() // Get final url
+        .html()
+        .then(function (html) {
+            callback(html);
+        })
+        // .log()
+        .close();
+
+    //TODO: Serialize crawl url
+
+
+    //TODO: Serialize failed search
+    return 0;
+}
+
+function checkStatus(status) {
+    if (status !== 200)
+    {
+        console.log("1st page does not load!");
+        horseman.close();
+        return 1;
+    }
+    return 0;
+}
+
+function checkSelectorExist(isSelectorExist) {
+    if(!isSelectorExist) {
+        horseman.close();
+        console.log('Selector does not exist!');
+        return 1;
+    }
+    return 0;
+}
+
+function serializeHtml(html) {
+    //TODO: Serialize html pages
+    // Use writeStream to write big file to disk
+    var writeStream = fs.createWriteStream(HTML_PATH);
+    writeStream
+        .on('open', function () {
+            console.log('---Writing HTML---');
+            writeStream.write(html);
+        })
+        .on('close', function () {
+            console.log('WriteStream finished!');
+        }); // Not printing
+    //TODO: Open file and check if it exists already or not.
+}
+
+if (require.main === module) {
+    var horseman = configHorseman(Horseman);
+    fsmHorseman(horseman, serializeHtml);
+}
+//http://data1.cde.ca.gov/dataquest/SearchName.asp?rbTimeFrame=oneyear&rYear=2015-16&cName=Franklin+Elementary&Topic=LC&Level=School&submit1=Submit
