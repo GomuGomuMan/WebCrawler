2676aebfa303b071c76e490ede808994d01b7bc1
GomuGomuMan <lucky7duy211@gmail.com>
2017-03-30 17:36:46 -0700

    Finished prototyping extracting original file

--- schoolList.js
+++ schoolList.js
@@ -5,10 +5,10 @@
 var csv = require('csv');
 var fs = require('graceful-fs');
 
-const CSV_PATH = 'resources/pubschls.csv';
-const JSON_PATH = 'output/schoolList.json';
-
-
+const CSV_PATH = 'resources/achieve schools- final.csv'; //resources/pubschls.csv
+const JSON_PATH = 'output/schoolList1.json'; //output/schoolList.json
+var school_col = 'el_school'; //School
+const REG = /[\w^)]$/;
 
 function extractSchool(err, data) {
     if (err)
@@ -17,9 +17,9 @@ function extractSchool(err, data) {
       var jsonSchool = [];
 
       data.forEach(function (row) {
-          //console.log("School: " + row.School);
-          if (row.School)
-            jsonSchool.push(row.School);
+          // if (row.el_school)
+          //   jsonSchool.push(row.el_school);
+          adjustStr(row[school_col], populateJSON);
       });
       console.log(jsonSchool);
 
@@ -32,9 +32,26 @@ function extractSchool(err, data) {
             else
                 console.log("schoolList write is done!");
         }
+
+        function populateJSON(str) {
+            jsonSchool.push(str);
+        }
+
     }
 }
 
+function adjustStr(str, callback)
+{
+    if(!str || REG.test(str)) // Check for empty input
+    {
+        return callback(str);
+    }
+    else
+    {
+        // console.log('Not match');
+        adjustStr(str.substring(0, str.length - 1), callback);
+    }
+}
 
 
 function readCSV() {
@@ -47,4 +64,8 @@ function readCSV() {
 
 if (require.main === module) {
     readCSV();
+    // readDatadump();
+    // var test = adjustStr('Cleminson Elementary', function (str) {
+    //     console.log(str);
+    // });
 }
