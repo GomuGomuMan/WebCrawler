dd5ce488cd3cc6ced4738a4b3efc03e20c9c17a2
GomuGomuMan <lucky7duy211@gmail.com>
2017-04-02 00:28:44 -0700

    Fixed Selector does not exist error + limit searchTerm's length to 25

--- crawler.js
+++ crawler.js
@@ -6,15 +6,18 @@ var Horseman = require('node-horseman');
 var fs = require('graceful-fs');
 var mkdirp = require('mkdirp');
 var JSONStream = require('JSONStream');
+var async = require('async');
 
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0';
-const URL = 'http://data1.cde.ca.gov/dataquest/page2.asp?level=School&subject=LC&submit1=Submit';
+const URL_PART1 = 'http://data1.cde.ca.gov/dataquest/SearchName.asp?rbTimeFrame=oneyear&rYear=2015-16&cName=';
+const URL_PART2 = '&Topic=LC&Level=School&submit1=Submit';
 const SELECTOR_SCHOOL = 'input[name="cName"]';
 const SELECTOR_FEP = 'input[value="Redesig4"]';
 const SELECTOR_SUBMIT = '#submit1';
 const JSON_PATH = "output/schoolList.json";
 
 var yearRange = generateYearRange();
+var failedSelector = [];
 
 
 function configHorseman(Horseman) {
@@ -25,18 +28,6 @@ function configHorseman(Horseman) {
 
     return horseman;
 }
-
-
-function checkSelectorExist(isSelectorExist) {
-    if(!isSelectorExist) {
-        horseman.close(); //TODO:Check
-        console.log('Selector does not exist!');
-        return 1;
-    }
-    return 0;
-}
-
-
 
 /*
 * Generate appropriate year range
@@ -100,6 +91,9 @@ function crawlYearRange(url, searchTerm) {
 }
 
 function assignTask(html, url, searchTerm) {
+
+    if (html === null || url === null || searchTerm === null)
+        return 1;
     serializeHtml(html, url, searchTerm);
 
     //TODO: Serialize crawl url
@@ -144,19 +138,126 @@ function serializeFailedURL(school, url, status) {
 * Reduces searchTerm to length 25 or less
 * */
 function shortenTerm(searchTerm) {
-    var idx = 24;
-    while(idx >= 0)
-    {
-        if (/\s/.test(searchTerm.charAt(idx))) {
-            // console.log("IDX: " + idx);
-            // console.log(searchTerm.substring(0, idx));
-            break;
-        }
-        --idx;
-    }
-    return searchTerm.substring(0, idx);
+    // var idx = 24;
+    // while(idx >= 0)
+    // {
+    //     if (/\s/.test(searchTerm.charAt(idx))) {
+    //         // console.log("IDX: " + idx);
+    //         // console.log(searchTerm.substring(0, idx));
+    //         break;
+    //     }
+    //     --idx;
+    // }
+    // return searchTerm.substring(0, idx);
+
+    // Return substr up to 25 length
+    //TODO: Replace substr's space with +
+
+
+    return searchTerm.substr(0, 25).replace(/\s/g, '+');
 }
 
+
+function fsmHorseman(horseman, searchTerm, callback) { // Finite State Machine Horseman
+    var shortenedTerm = shortenTerm(searchTerm);
+
+    horseman
+        .userAgent(USER_AGENT)
+        .open(URL_PART1 + shortenedTerm + URL_PART2)
+        .status()
+        .then(checkStatus)
+        //1st page
+        // Check FEP
+        .exists(SELECTOR_FEP)
+        .then(function (isSelectorExist) {
+            if (!isSelectorExist) {
+                console.log("--Error--");
+                console.log('Selector does not exist!');
+                //TODO: Serialize failed url
+                horseman
+                    .url()
+                    .then(function (url) {
+                        console.log("selector NOT found at " + url);
+                        var obj = {
+                            school: searchTerm,
+                            url: url,
+                            error: 'selectorNotFound'
+                        };
+                        failedSelector.push(obj);
+                        console.log(failedSelector);
+
+
+                    })
+                    .close();
+                return callback(null, null, null);
+            }
+            horseman
+                .click(SELECTOR_FEP)
+                // Click submit btn
+                .exists(SELECTOR_SUBMIT)
+                .then(checkSelectorExist)
+                .click(SELECTOR_SUBMIT)
+                .waitForNextPage()
+                .status()
+                .then(checkStatus)
+                //result page
+                .screenshot('test.png') // Success!!
+                .html()
+                .then(function (html) {
+                    horseman
+                        .url()
+                        .then(function (url) {
+                            console.log("--Processing--");
+                            console.log("word: " + searchTerm);
+                            console.log("url: " + url);
+                            callback(html, url, searchTerm);
+                        })
+                        .close();
+                });
+        });
+
+    //TODO: Serialize failed search
+
+
+    function checkStatus(status) {
+        if (status !== 200) {
+            console.log("--Error--");
+            console.log("Status: " + status);
+            horseman
+                .url()
+                .then(function (url) {
+                    console.log("URL: " + url);
+                    //TODO: Better option is csv or json?
+                    serializeFailedURL(searchTerm, url, status);
+                })
+        }
+    }
+
+    return 0;
+}
+
+function checkSelectorExist(isSelectorExist) {
+    if(!isSelectorExist) {
+        horseman.close(); //TODO:Check
+        console.log('Selector does not exist!');
+        return 1;
+    }
+    return 0;
+}
+
+function serializeHtml(html, url, searchTerm) {
+    //TODO: Serialize html pages
+    var yearRange = url.substring(url.length - 7);
+    var customFileName = 'output/' + searchTerm + '/' + yearRange + '.html';
+
+    // Use writeStream to write big file to disk
+    var writeStream = fs.createWriteStream(customFileName);
+    writeStream.write(html);
+    writeStream.end();
+
+    //TODO: Open file and check if it exists already or not.
+
+}
 
 if (require.main === module) {
     // var temp = {
@@ -172,91 +273,13 @@ if (require.main === module) {
     //     console.log(data);
     // });
 
-    var searchTerm = 'Wheatland Alternative Education';
-    var shortenedTerm = shortenTerm(searchTerm);
+    var searchTerm = 'T. S. MacQuiddy Elementary';
 
     var horseman = configHorseman(Horseman);
 
-    var isCreated = makeDir(shortenedTerm);
+    var isCreated = makeDir(searchTerm);
     if (isCreated) {
-        fsmHorseman(horseman, shortenedTerm, assignTask);
-    }
-
-
-    function fsmHorseman(horseman, searchTerm, callback) { // Finite State Machine Horseman
-        horseman
-            .userAgent(USER_AGENT)
-            .open(URL)
-            .status()
-            .then(checkStatus)
-            //1st page
-            .exists(SELECTOR_SCHOOL)
-            .then(checkSelectorExist)
-            .type(SELECTOR_SCHOOL, searchTerm) // input school //TODO: limit to 25 char and text from json
-            .keyboardEvent('keypress', 16777221) // type enter
-            .waitForNextPage() // wait for loading
-            .status()
-            .then(checkStatus)
-            //2nd page
-            // Check FEP
-            .exists(SELECTOR_FEP)
-            .then(checkSelectorExist)
-            .click(SELECTOR_FEP)
-            // Click submit btn
-            .exists(SELECTOR_SUBMIT)
-            .then(checkSelectorExist)
-            .click(SELECTOR_SUBMIT)
-            .waitForNextPage()
-            .status()
-            .then(checkStatus)
-            //result page
-            .screenshot('test.png') // Success!!
-            .html()
-            .then(function (html) {
-                horseman
-                    .url()
-                    .then(function (url) {
-                        console.log("--Processing--");
-                        console.log("word: " + searchTerm);
-                        console.log("url: " + url);
-                        callback(html, url, searchTerm);
-                    })
-                    .close();
-            });
-        //TODO: Serialize failed search
-
-
-        function checkStatus(status) {
-            if (status !== 200) {
-                console.log("--Error--");
-                console.log("Status: " + status);
-                horseman
-                    .url()
-                    .then(function (url) {
-                        console.log("URL: " + url);
-                        //TODO: Better option is csv or json?
-                        serializeFailedURL(searchTerm, url, status);
-                    })
-            }
-        }
-
-        return 0;
-    }
-
-
-
-    function serializeHtml(html, url, searchTerm) {
-        //TODO: Serialize html pages
-        var yearRange = url.substring(url.length - 7);
-        var customFileName = 'output/' + searchTerm + '/' + yearRange + '.html';
-
-        // Use writeStream to write big file to disk
-        var writeStream = fs.createWriteStream(customFileName);
-        writeStream.write(html);
-        writeStream.end();
-
-        //TODO: Open file and check if it exists already or not.
-
+        fsmHorseman(horseman, searchTerm, assignTask);
     }
 }
 
